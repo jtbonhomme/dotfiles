69c6f180f83daa7562ec60b18b16bf7df010b781
Jean-Thierry BONHOMME <jtbonhomme@gmail.com>
2021-11-02 09:21:37 +0100

    update install

--- install.sh
+++ install.sh
@@ -97,15 +97,47 @@ mkdir -p ~/Documents/Developments/golang/src
 # iterm
 curl -o ~/Downloads/iTerm2-3_4_4.zip https://iterm2.com/downloads/stable/iTerm2-3_4_4.zip
 unzip ~/Downloads/iTerm2-3_4_4.zip -d /Applications
-rm ~/Downloads/iTerm2-3_4_4.zip
+#rm ~/Downloads/iTerm2-3_4_4.zip
 
 # aws cli
 curl -o ~/Downloads/AWSCLIV2.pkg https://awscli.amazonaws.com/AWSCLIV2.pkg
 sudo installer -pkg ~/Downloads/AWSCLIV2.pkg  -target /
-rm ~/Downloads/AWSCLIV2.pkg
+#rm ~/Downloads/AWSCLIV2.pkg
 
 # docker
 curl -o ~/Downloads/Docker.dmg https://desktop.docker.com/mac/stable/Docker.dmg
+
+# Caffeine.app
+curl -o ~/Downloads/Caffeine.dmg https://github.com/IntelliScape/caffeine/releases/download/1.1.3/Caffeine.dmg
+# Conduktor.app
+curl -o ~/Downloads/Conduktor-2.18.5.pkg https://github.com/conduktor/builds/releases/download/v2.18.5/Conduktor-2.18.5.pkg
+sudo installer -pkg ~/Downloads/Conduktor-2.18.5.pkg  -target /
+
+# DataGrip.app
+curl ~/Downloads/datagrip-2021.2.4.dm https://download.jetbrains.com/datagrip/datagrip-2021.2.4.dmg?_gl=1*ouoe2p*_ga*MzQ4MTc5MTAzLjE2MzU4NDEyMjA.*_ga_V0XZL7QHEB*MTYzNTg0MTIxOS4xLjEuMTYzNTg0MTIzMS4w&_ga=2.128536780.1711659072.1635841220-348179103.1635841220
+# Dropbox.app
+open https://www.dropbox.com/fr/downloading
+# Google Chrome.app
+# Itsycal.app
+curl -o ~/Downloads/Itsycal.zip  https://itsycal.s3.amazonaws.com/Itsycal.zip
+unzip ~/Downloads/Itsycal.zip -d /Applications
+# Miro.app
+curl -o ~/Downloads/Miro.dmg https://desktop.miro.com/platforms/darwin/Miro.dmg
+# Notion.app
+curl -o ~/Downloads/Notion.dmg https://desktop-release.notion-static.com/Notion-2.0.19.dmg
+# Pritunl.app
+# Visual Studio Code.app
+open https://code.visualstudio.com/Download
+
+## APP STORE
+# WhatsApp.app 
+# Clipboard.app
+# Todoist.app
+# Magnet.app
+# Slack.app
+# Bitwarden.app
+
+
 
 # At the end
 
